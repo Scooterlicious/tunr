38cc9da8b2598778fff76ad56fc1774afc5da0d2
Scooterlicious <ScottDavidFell@gmail.com>
2014-01-11 21:02:42 -0500

    All tests added to Bands.  Bootstrap added.  All tests pass

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,3 +1,12 @@
+#     bands GET    /bands(.:format)                     bands#index
+#           POST   /bands(.:format)                     bands#create
+#  new_band GET    /bands/new(.:format)                 bands#new
+# edit_band GET    /bands/:id/edit(.:format)            bands#edit
+#      band GET    /bands/:id(.:format)                 bands#show
+#           PUT    /bands/:id(.:format)                 bands#update
+#           DELETE /bands/:id(.:format)                 bands#destroy
+
+
 class BandsController < ApplicationController
 	def index
 		@bands = Band.all
@@ -12,4 +21,27 @@ class BandsController < ApplicationController
 		})
 		redirect_to bands_path
 	end
+
+
+	def show
+		@band = Band.find(params[:id])
+	end	
+
+	def destroy 
+		Band.find(params[:id]).destroy
+		redirect_to bands_path
+	end	
+
+	def edit
+		@band = Band.find(params[:id])
+	end
+
+	def update
+		@band = Band.find(params[:id])
+		@band.name = params[:band][:name]
+		@band.save
+		redirect_to band_path(params[:id])
+	end
+
+	
 end
--- spec/features/bands_feature_spec.rb
+++ spec/features/bands_feature_spec.rb
@@ -19,4 +19,58 @@ describe BandsController do
 			end
 		end
 	end
+
+
+	describe "Given a Band" do
+		before do
+			@band = Band.create(name: "Dummy")
+		end
+		describe "when we visit the edit page" do
+			before do
+				visit edit_band_path(@band)
+			end 
+			it "should have the bands name" do
+				find_field("band_name").value.should == @band.name
+			end
+			describe "when we edit the band_name" do
+				before do
+					fill_in "band_name", with: "Diana"
+					click_button "submit"
+				end
+				it "should take us to the show page" do
+					current_path.should == band_path(@band)
+				end
+				it "should have the new name" do
+					page.should have_content("Diana")
+				end
+			end
+		end
+	
+		describe "when we visit the index page" do
+			before do
+				visit bands_path
+			end
+			describe "when we click the delete button" do
+				before do
+					within "tr.band_#{@band.id}" do
+						click_button "DELETE"
+					end
+				end
+				it "should leave us on the index page" do
+					#This is bizarre: IT should be just fine...  When I do this interactively, it lands me back on the /bands page, and the bands_path should be exactly that.
+					#current_path.should == bands_path
+					current_path.should == '/bands'
+				end
+				it "should no longer have the band" do
+					page.should_not have_content(@band.name)
+				end
+			end
+		end
+	end
+
+
+
+
+
+
 end
